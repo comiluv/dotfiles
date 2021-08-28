8b3d2fb6c7a549bc63a620f59551886cfc908ab4
comiluv <choijaeho@gmail.com>
2021-08-28 09:32:55 +0900

    add lsp #2

---
+++ .vim/vim-include/null-ls-config.lua
@@ -0,0 +1,11 @@
+-- example configuration! (see CONFIG above to make your own)
+require("null-ls").config({
+	sources = { require("null-ls").builtins.formatting.stylua },
+})
+require("lspconfig")["null-ls"].setup({
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+		end
+	end,
+})
