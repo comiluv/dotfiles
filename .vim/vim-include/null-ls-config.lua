c1f993b8dd4e44e0a9abaa30c3f849883da87053
comiluv <choijaeho@gmail.com>
2022-05-05 01:58:01 +0900

    update null-ls config

--- .vim/vim-include/null-ls-config.lua
+++ .vim/vim-include/null-ls-config.lua
@@ -1,11 +1,8 @@
 -- example configuration! (see CONFIG above to make your own)
-require("null-ls").config({
-	sources = { require("null-ls").builtins.formatting.stylua },
-})
-require("lspconfig")["null-ls"].setup({
-	-- on_attach = function(client)
-	-- 	if client.resolved_capabilities.document_formatting then
-	-- 		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-	-- 	end
-	-- end,
+require("null-ls").setup({
+    sources = {
+        require("null-ls").builtins.formatting.stylua,
+        require("null-ls").builtins.diagnostics.eslint,
+        require("null-ls").builtins.completion.spell,
+    },
 })
