cbddda0682ca2172712b8075c1daf7b66b521766
comiluv <choijaeho@gmail.com>
2023-02-05 06:54:06 +0900

    compile and run hotkeys for c and python

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -1,17 +1,17 @@
 -- it'll also be used for C++
 
--- make a keymap for F5 to invoke make on the file
-vim.keymap.set("n", "<F5>", ":<C-u>w!<bar>!make %<<CR>", { buffer = true })
+-- pressing F5 will call make to compile it
+vim.keymap.set("n", "<F5>", ":<C-u>w!<CR>:exec '!make' shellescape(expand('%<'))<CR>", { buffer = true })
 
 -- pressing F5 key in insert mode or visual mode will exit respective mode
 -- and press F5 in normal mode
 vim.keymap.set({ "i", "v" }, "<F5>", "<ESC><F5>", { remap = true })
 
--- pressing f8 will invoke command :<C-u>term ./%<CR>
-if vim.fn.has("macunix") == true then
-	vim.keymap.set("n", "<f8>", ":<C-u>term ./%<CR>", { buffer = true })
+-- pressing f8 will run the executable
+if vim.fn.has("win32") then
+    vim.keymap.set("n", "<f8>", ":<C-u>exec 'term' shellescape(expand('%<')..'.exe')<CR>", { buffer = true })
 else
-	vim.keymap.set("n", "<f8>", ":<C-u>term %<.exe<CR>", { buffer = true })
+    vim.keymap.set("n", "<f8>", ":<C-u>exec 'term' shellescape('./'..expand('%<'))<CR>", { buffer = true })
 end
 
 -- just like above, pressing F8 in insert mode or visual mode will exit respective
