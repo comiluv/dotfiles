eb6212e0ed90a29d303b602b76f432a45161553b
comiluv <choijaeho@gmail.com>
2023-02-07 06:55:23 +0900

    fix keymap desc

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {}, {desc = "Find files"})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc = "Find files"})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {}, {desc = "Find git files"})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
