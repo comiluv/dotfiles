0c547d6b95a3bb1b4a307d5aa50d333a9f072852
comiluv <choijaeho@gmail.com>
2023-02-09 21:20:29 +0900

    fix tele-fzf-native from wsl side

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,6 @@
-local ok, builtin = pcall(require,'telescope.builtin')
+local ok, telescope = pcall(require,'telescope')
 if not ok then return end
+local builtin = require'telescope.builtin'
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc = "Find files"})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {}, {desc = "Find git files"})
@@ -7,6 +8,6 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end, {desc = "Grep string"})
 
-require'telescope'.setup{}
-require'telescope'.load_extension('fzf')
+telescope.setup{}
+telescope.load_extension('fzf')
 
