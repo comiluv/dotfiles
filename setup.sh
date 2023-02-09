289340d87e55a1925de1062e4252fd850ffe0310
comiluv <choijaeho@gmail.com>
2023-02-09 16:23:45 +0900

    remove unnecessary line breaks

--- setup.sh
+++ setup.sh
@@ -84,7 +84,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 # copy paste NVM environment vars to .zshrc
 cat >> ~/.zshrc << EOF
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
