660a21c8aa8224c591ddc245f61e9781f3285cb1
comiluv <choijaeho@gmail.com>
2023-02-12 23:10:56 +0900

    integrate cmp, luasnip, and codeium fns to Tab key more luasnip configs

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -45,6 +45,39 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
+-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+local luasnip = require'luasnip'
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+cmp_mappings['<Tab>'] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+        cmp.select_next_item()
+        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+        -- they way you will only jump inside the snippet region
+    elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+    elseif has_words_before() then
+        cmp.complete()
+        -- check if codeium is enabled and try to use codeium data
+    elseif vim.g.codeium_enabled == true then
+        vim.api.nvim_feedkeys(fn['codeium#Accept'](), 'i', true)
+    else
+        fallback()
+    end
+end, {"i","s"})
+cmp_mappings['<S-Tab>'] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+        cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+    else
+        fallback()
+    end
+end, { "i", "s" })
+
 -- fix lsp-zero auto selecting first item: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v1.x/advance-usage.md#dont-preselect-first-match
 lsp.setup_nvim_cmp({
     preselect = cmp.PreselectMode.None,
---
+++ nvim/after/plugin/luasnip.lua
@@ -0,0 +1,18 @@
+-- https://github.com/L3MON4D3/LuaSnip/wiki/Nice-Configs
+local types = require("luasnip.util.types")
+
+require'luasnip'.config.setup({
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = {{"●", "GruvboxOrange"}}
+			}
+		},
+		[types.insertNode] = {
+			active = {
+				virt_text = {{"●", "GruvboxBlue"}}
+			}
+		}
+	},
+})
+
