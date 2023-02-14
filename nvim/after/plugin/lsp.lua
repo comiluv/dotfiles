001af9597c9e17b5ea35871175f3e756aa47a499
comiluv <choijaeho@gmail.com>
2023-02-15 01:50:55 +0900

    lsp hotkeys

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -83,25 +83,29 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(client, bufnr)
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end,
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition,
     { buffer = bufnr, desc = "Go to definition"})
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end,
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation,
+    { buffer = bufnr, desc = "Go to implementation"})
+    vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references(),
+    { buffer = bufnr, desc = "Go to references"})
+    vim.keymap.set("n", "K", vim.lsp.buf.hover,
     { buffer = bufnr, })
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end,
+    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol,
     { buffer = bufnr, desc = "Workspace symbol"})
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end,
+    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float,
     { buffer = bufnr, desc = "View diagnostic"})
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end,
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next,
     { buffer = bufnr, desc = "Next diagnostic"})
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end,
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev,
     { buffer = bufnr, desc = "Previous diagnostic"})
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end,
+    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action,
     { buffer = bufnr, desc = "Code action"})
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end,
+    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references,
     { buffer = bufnr, desc = "Open references"})
     vim.keymap.set("n", "<leader>vrn", ":IncRename <C-r><C-w>",
     { buffer = bufnr, desc = "Rename symbol"})
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end,
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help,
     { buffer = bufnr, desc = "Signature help"})
 
     vim.api.nvim_create_autocmd("CursorHold", {
