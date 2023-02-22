8031b502e62fffbdd343e739a35ac181b37f0ea8
comiluv <choijaeho@gmail.com>
2023-02-23 06:09:01 +0900

    more detailed comments

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -13,9 +13,14 @@ List of plugins that Neovim version is desired
 	www.github.com/junegunn/vim-easy-align : lua could be faster
 }
 ]]--
-local OpenedBuffer = { "BufRead", "BufNewFile", } -- note new buffer opened by :enew
+local OpenedBuffer = { "BufRead", "BufNewFile", } -- note the new buffer opened by :enew
                                                   -- doesn't trigger neighther of the events
-
+												  -- but keep it this way because other events
+												  -- such as "CursorHold", "CursorMoved", are
+												  -- triggered as soon as Netrw is opened: not what I want.
+												  -- Adding "InsertEnter" is to be avoided because
+												  -- Telescope triggers "InsertEnter" event and I want to
+												  -- seperate plugin loading to "BufRead" and "InsertEnter"
 return {
 	{
 		"nvim-telescope/telescope.nvim",
