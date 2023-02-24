3e08db81b81e359c3be5ba84c2076bb6ebbf446c
comiluv <choijaeho@gmail.com>
2023-02-24 15:07:31 +0900

    disable shadafile when sourcing config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,11 @@
 -- note placing files in /plugin directory did NOT improve start up time
 -- this structure is about 10 ms faster in hyperfine results
+vim.opt.shadafile = "NONE"
+
 require("01_set")
 require("02_remap")
 require("03_autocmd")
 require("04_load_lazy")
+
+vim.opt.shadafile = ""
 
