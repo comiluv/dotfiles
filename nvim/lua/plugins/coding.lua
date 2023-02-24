cfed343d7e59b4ef599633bad63d90bcf611d042
comiluv <choijaeho@gmail.com>
2023-02-24 20:04:24 +0900

    proper vim-abolish which-key support

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -118,6 +118,24 @@ return {
 		"tpope/vim-abolish",
 		cmd = { "Abolish", "Subvert", "S" },
 		keys = "cr",
+		config = function()
+			require("which-key").register({
+				cr = {
+					name = "+coercion",
+					s = { desc = "snake_case" },
+					_ = { desc = "snake_case" },
+					m = { desc = "MixedCase" },
+					c = { desc = "camelCase" },
+					u = { desc = "SNAKE_UPPER_CASE" },
+					U = { desc = "SNAKE_UPPER_CASE" },
+					k = { desc = "kebab-case" },
+					t = { desc = "Title Case (not reversible)" },
+					["-"] = { desc = "kebab-case (not reversible)" },
+					["."] = { desc = "dot.case (not reversible)" },
+					["<space>"] = { desc = "space case (not reversible)" },
+				}
+			})
+		end
 	},
 
 	-- LSP incrementally rename symbol
