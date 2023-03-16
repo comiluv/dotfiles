85894403458e08a59dde2c9d91ce379bf842450b
comiluv <choijaeho@gmail.com>
2023-03-16 19:23:18 +0900

    remove alpha

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -15,7 +15,6 @@ return {
 			enabled = true,
 			filetype_exclude = {
 				"help",
-				"alpha",
 				"dashboard",
 				"lazy",
 				"Trouble",
@@ -35,35 +34,6 @@ return {
 		event = { "BufRead", "BufNewFile" },
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = true,
-	},
-
-	-- dashboard
-	{
-		"goolord/alpha-nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local alpha = require("alpha")
-			local startify = require("alpha.themes.startify")
-			-- Powershell fortune: https://www.bgreco.net/fortune
-			-- fortune.txt.dat is produced in WSL
-			--[[ local handle
-				if vim.fn.has('win32') == 1 then
-					handle = assert(io.popen("fortune.ps1|cowsay -W 120 --random", "r"))
-				else
-					handle = assert(io.popen("fortune|cowsay -W 120 --random", "r"))
-				end
-				local fortune_raw = assert(handle:read("*a"))
-				handle:close()
-
-				local fortune = {}
-				for s in string.gmatch(fortune_raw, "[^\r\n]+") do
-					table.insert(fortune, s)
-				end
-
-				startify.section.header.val = fortune ]]
-			alpha.setup(startify.config)
-		end,
 	},
 
 	-- history
