66f875b6d4a947224b016766a2f76aea3c1e25fa
comiluv <choijaeho@gmail.com>
2023-03-21 10:54:16 +0900

    fix lazy.nvim setup

--- nvim/lua/04_load_lazy.lua
+++ nvim/lua/04_load_lazy.lua
@@ -15,9 +15,43 @@ vim.opt.rtp:prepend(lazypath)
 -- fix drag-n-drop functionality for neovim-qt
 -- see https://github.com/folke/lazy.nvim/issues/403
 local opts = {
+	change_detection = {
+		notify = false,
+	},
 	performance = {
 		rtp = {
 			reset = false,
+			disabled_plugins = {
+				"2html_plugin",
+				"bugreport",
+				"compiler",
+				"did_load_filetypes",
+				"ftplugin",
+				"getscript",
+				"getscriptPlugin",
+				"gzip",
+				"logipat",
+				"man",
+				"matchit",
+				"matchparen",
+				"optwin",
+				"perl_provider",
+				"rplugin",
+				"rrhelper",
+				"ruby_provider",
+				"spellfile",
+				"spellfile_plugin",
+				"synmenu",
+				"syntax",
+				"tar",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"vimball",
+				"vimballPlugin",
+				"zip",
+				"zipPlugin",
+			},
 		},
 	},
 }
