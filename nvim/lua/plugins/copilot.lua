0d86be965a5603f101ab6686b437121136911f9a
comiluv <choijaeho@gmail.com>
2023-03-24 14:24:09 +0900

    fix copilot setup

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -13,6 +13,10 @@ return {
 					},
 				},
 				filetypes = {
+					-- https://codeinthehole.com/tips/vim-and-github-copilot/
+					gitcommit = true,
+					markdown = true,
+					yaml = true,
 					qf = false,
 					notify = false,
 					lspinfo = false,
@@ -26,10 +30,26 @@ return {
 					tsplaground = false,
 				},
 			})
-			vim.cmd("Copilot disable")
+			-- detach Copilot for big files
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("CopilotFileSizeCheck", {}),
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if client.name == "copilot" then
+						local file_size = vim.fn.getfsize(args.file)
+						if file_size > 100000 or file_size == -2 then
+							vim.defer_fn(function()
+								vim.cmd("silent Copilot detach")
+							end, 0)
+						end
+					end
+				end,
+			})
+			-- start Copilot disabled
+			vim.cmd("silent Copilot disable")
 		end,
 	},
-	-- Packer
+
 	{
 		-- see https://github.com/microsoft/terminal/issues/530#issuecomment-755917602
 		-- https://zshchun.github.io/posts/windows-terminal-wsl2-%EC%97%90%EC%84%9C-shift-enter-%EC%9E%85%EB%A0%A5%EB%B0%A9%EB%B2%95/
@@ -38,7 +58,7 @@ return {
 		"comiluv/ChatGPT.nvim",
 		cmd = { "ChatGPT", "ChatGPTActAs", "ChatGPTEditWithInstructions" },
 		config = function()
-			require("chatgpt").setup({ })
+			require("chatgpt").setup({})
 		end,
 		dependencies = {
 			"MunifTanjim/nui.nvim",
