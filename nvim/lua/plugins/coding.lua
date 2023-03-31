4135ae08edb37f112178e92481b59f8e6ee52179
comiluv <choijaeho@gmail.com>
2023-04-01 01:03:38 +0900

    remove vim-abolish because actually I'm not using it

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -103,32 +103,6 @@ return {
 		end,
 	},
 
-	-- search for, substitute, coerce, and abbreviate variants of a word
-	{
-		"tpope/vim-abolish",
-		event = "VeryLazy",
-		cmd = { "Abolish", "Subvert", "S" },
-		config = function()
-			require("which-key").register({
-				cr = {
-					name = "+coercion",
-					s = { desc = "snake_case" },
-					_ = { desc = "snake_case" },
-					m = { desc = "MixedCase" },
-					p = { desc = "PascalCase" },
-					c = { desc = "camelCase" },
-					u = { desc = "SNAKE_UPPER_CASE" },
-					U = { desc = "SNAKE_UPPER_CASE" },
-					k = { desc = "kebab-case" },
-					t = { desc = "Title Case (not reversible)" },
-					["-"] = { desc = "kebab-case (not reversible)" },
-					["."] = { desc = "dot.case (not reversible)" },
-					["<space>"] = { desc = "space case (not reversible)" },
-				}
-			})
-		end
-	},
-
 	-- LSP incrementally rename symbol
 	{
 		"smjonas/inc-rename.nvim",
