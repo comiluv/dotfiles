fa5fbc3a8dc06a0a08387ceeaf6d8d1e7e0c0e4c
comiluv <choijaeho@gmail.com>
2023-04-02 07:22:43 +0900

    prevent jdtls from formatting

--- nvim/after/ftplugin/java.lua
+++ nvim/after/ftplugin/java.lua
@@ -12,18 +12,48 @@ vim.keymap.set({ "i", "v" }, "<F5>", "<ESC><F5>", { buffer = true, remap = true 
 vim.keymap.set("n", "<F8>", ":<C-u>cd %:p:h<BAR>te java %<<CR>", { buffer = true })
 vim.keymap.set({ "i", "v" }, "<F8>", "<ESC><F8>", { buffer = true, remap = true })
 
+vim.keymap.set("n", "<leader>f", function()
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name ~= "jdtls"
+		end,
+		async = true,
+		timeout_ms = 10000,
+	})
+end, { buffer = true, desc = "Format buffer" })
+
 -- https://github.com/mfussenegger/nvim-jdtls#Configuration-verbose
 -- Only verbose config version works. Quickstart doens't work
 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace_dir = 'C:\\code\\java_headfirst\\' .. project_name
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = "C:\\code\\java_headfirst\\" .. project_name
 
-local jdtls_dir = vim.fn.stdpath('data') .. '/mason/packages/jdtls'
-local config_dir = jdtls_dir .. '/config_win'
-local plugins_dir = jdtls_dir .. '/plugins'
-local path_to_jar = plugins_dir .. '/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar'
+local jdtls_dir = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
+local config_dir = jdtls_dir .. "/config_win"
+local plugins_dir = jdtls_dir .. "/plugins"
+local path_to_jar = plugins_dir .. "/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar"
 local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
 local root_dir = require("jdtls.setup").find_root(root_markers)
+local capabilities = {
+	workspace = {
+		configuration = true,
+	},
+	textDocument = {
+		completion = {
+			completionItem = {
+				snippetSupport = true,
+			},
+		},
+	},
+}
+
+-- remaps are handled globally by LspAttach autocmd
+local lsp_onattach = function(client, bufnr)
+	-- get nvim-navic working with multiple tabs
+	if client.server_capabilities["documentSymbolProvider"] then
+		require("nvim-navic").attach(client, bufnr)
+	end
+end
 
 local config = {
 	-- The command that starts the language server
@@ -31,36 +61,39 @@ local config = {
 	cmd = {
 
 		-- 💀
-		'java', -- or '/path/to/java17_or_newer/bin/java'
+		"java", -- or '/path/to/java17_or_newer/bin/java'
 		-- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
-		'-Declipse.application=org.eclipse.jdt.ls.core.id1',
-		'-Dosgi.bundles.defaultStartLevel=4',
-		'-Declipse.product=org.eclipse.jdt.ls.core.product',
-		'-Dlog.protocol=true',
-		'-Dlog.level=ALL',
-		'-Xms1g',
-		'--add-modules=ALL-SYSTEM',
-		'--add-opens', 'java.base/java.util=ALL-UNNAMED',
-		'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-Xms1g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
 
 		-- 💀
-		'-jar', path_to_jar,
+		"-jar",
+		path_to_jar,
 		-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
 		-- Must point to the                                                     Change this to
 		-- eclipse.jdt.ls installation                                           the actual version
 
-
 		-- 💀
-		'-configuration', config_dir,
+		"-configuration",
+		config_dir,
 		-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
 		-- Must point to the                      Change to one of `linux`, `win` or `mac`
 		-- eclipse.jdt.ls installation            Depending on your system.
 
-
 		-- 💀
 		-- See `data directory configuration` section in the README
-		'-data', workspace_dir,
+		"-data",
+		workspace_dir,
 	},
 	-- 💀
 	-- This is the default if not provided, you can remove it. Or adjust as needed.
@@ -72,7 +105,8 @@ local config = {
 	settings = {
 		java = {
 			signatureHelp = { enabled = true },
-		}
+			format = { enabled = false },
+		},
 	},
 	-- Language server `initializationOptions`
 	-- You need to extend the `bundles` with paths to jar files
@@ -82,19 +116,13 @@ local config = {
 	--
 	-- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
 	init_options = {
-		bundles = {}
+		bundles = {},
 	},
+	capabilities = capabilities,
+	on_attach = lsp_onattach,
 }
-
--- remaps are handled globally by LspAttach autocmd
-config['on_attach'] = function(client, bufnr)
-	-- get nvim-navic working with multiple tabs
-	if client.server_capabilities["documentSymbolProvider"] then
-		require("nvim-navic").attach(client, bufnr)
-	end
-end
 
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
-require('jdtls').start_or_attach(config)
+require("jdtls").start_or_attach(config)
 
