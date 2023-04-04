ac2065a9c4e85d402a613d211856151ab9c2a1bd
comiluv <choijaeho@gmail.com>
2023-04-04 22:25:17 +0900

    run lua with f8

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -4,3 +4,6 @@ vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
 vim.bo.expandtab = false
 
+-- pressing f8 will run the file
+vim.keymap.set("n", "<F8>", ":<C-u>update<BAR>:cd %:p:h<BAR>:exec 'term lua' shellescape(@%)<CR>", { buffer = true })
+vim.keymap.set({ "i", "v" }, "<F8>", "<ESC><F8>", { buffer = true, remap = true })
