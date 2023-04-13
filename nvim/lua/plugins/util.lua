dc3e886304dd675695f610ad6b117e243eb1a3ea
comiluv <choijaeho@gmail.com>
2023-04-13 11:12:04 +0900

    use BufEnter event for mkdir.nvim

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -2,6 +2,7 @@ return {
 	-- automatically create any non-existent directories
 	{
 		"jghauser/mkdir.nvim",
-		event = "VeryLazy", -- "BufWritePre", "FileWritePre", "BufWriteCmd", "BufModifiedSet", didnt work
+		event = "BufEnter",
+		-- event = "VeryLazy", -- "BufWritePre", "FileWritePre", "BufWriteCmd", "BufModifiedSet", didnt work
 	},
 }
