04bcf2fc93d1e92e9ba1c5b7191666f1ab4abc6c
comiluv <choijaeho@gmail.com>
2023-04-15 19:17:04 +0900

    install open.nvim

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -5,4 +5,20 @@ return {
 		event = "BufEnter",
 		-- event = "VeryLazy", -- "BufWritePre", "FileWritePre", "BufWriteCmd", "BufModifiedSet", didnt work
 	},
+
+	-- Open the current word with custom openers, GitHub shorthands for example.
+	{
+		"ofirgall/open.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+		keys = {
+			{
+				"gx",
+				function()
+					require("open").open_cword()
+				end,
+				desc = "Open cursor using system default program",
+			},
+		},
+		opts = { curl_opts = { compressed = false } },
+	},
 }
