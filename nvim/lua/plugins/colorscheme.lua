696448e3682be0d5d36b5003ec4f68b1872cb7a8
comiluv <choijaeho@gmail.com>
2023-05-10 13:54:36 +0900

    remove colorschemes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,39 +1,6 @@
 return {
 	{
-		"navarasu/onedark.nvim",
-		lazy = true,
-		config = function()
-			require("onedark").setup()
-			require("onedark").load()
-		end,
-	},
-
-	{
-		"Tsuzat/NeoSolarized.nvim",
-		lazy = true,
-		opts = { transparent = false },
-	},
-
-	{
-		"tanvirtin/monokai.nvim",
-		lazy = true,
-	},
-
-	{
-		"luisiacc/gruvbox-baby",
-		config = function()
-			vim.cmd.colorscheme("gruvbox-baby")
-		end,
-	},
-
-	{
-		"Mofiqul/dracula.nvim",
-		lazy = true,
-	},
-
-	{
 		"Mofiqul/vscode.nvim",
-		lazy = true,
 		config = function()
 			require("vscode").setup({
 				transparent = false,
