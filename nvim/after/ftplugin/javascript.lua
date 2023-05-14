8ae36e92ed9b927938a3941dcb39c260b12124a1
comiluv <choijaeho@gmail.com>
2023-05-15 00:18:16 +0900

    ftplugin for javascript

---
+++ nvim/after/ftplugin/javascript.lua
@@ -0,0 +1,11 @@
+-- As suggested by prettier
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.softtabstop = 2
+vim.bo.expandtab = true
+
+-- pressing f8 will run the file and skip vim.fn.has("node") test for performance
+vim.keymap.set("n", "<F8>", ":<C-u>update<BAR>:cd %:p:h<BAR>:exec 'term node' shellescape(@%)<CR>", { buffer = true })
+
+-- pressing F8 in insert mode or visual mode will exit respective mode and press F8
+vim.keymap.set({ "i", "v" }, "<F8>", "<ESC><F8>", { buffer = true, remap = true })
