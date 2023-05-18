777893fdb92a30ae9da0d8ebafefde85e6cb7092
comiluv <choijaeho@gmail.com>
2023-05-19 01:56:03 +0900

    Neovim Microsoft Visual Studio support

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -21,4 +21,20 @@ return {
 		},
 		opts = { curl_opts = { compressed = false } },
 	},
+
+	-- Use Microsoft Visual Studio as default C/C++ compiler in Neovim
+	{
+		"hattya/vcvars.vim",
+		event = "VeryLazy",
+		config = function()
+			-- refernce: https://stackoverflow.com/questions/14369032/how-do-i-set-up-vim-to-compile-using-visual-studio-2010s-c-compiler
+			vim.cmd([[
+			let vcvars_dict = vcvars#get(vcvars#list()[-1])
+			let $PATH = join(vcvars_dict.path, ';') .';'. $PATH
+			let $INCLUDE = join(vcvars_dict.include, ';') .';'. $INCLUDE
+			let $LIB = join(vcvars_dict.lib, ';') .';'. $LIB
+			let $LIBPATH = join(vcvars_dict.libpath, ';') .';'. $LIBPATH
+			]])
+		end,
+	},
 }
