85a8422020d41e43d92b5ce91ab266954df18348
comiluv <choijaeho@gmail.com>
2023-05-22 04:52:04 +0900

    fix c ftplugin

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -12,7 +12,13 @@ if vim.fn.has("win32") == 1 then
 end
 
 -- pressing F5 will call make to compile it
-vim.keymap.set("n", "<F5>", ":<C-u>cd %:p:h<BAR>:update<BAR>make %<<CR>", { buffer = true })
+vim.keymap.set("n", "<F5>", function()
+	vim.cmd.cd("%:p:h")
+	vim.cmd.call("mkdir('obj','p')")
+	vim.cmd.call("mkdir('bin','p')")
+	vim.cmd.update()
+	vim.cmd("!cl.exe /Wall /Za /Fo.\\obj\\ /Fe.\\bin\\ %")
+end, { buffer = true })
 
 -- pressing F5 key in insert mode or visual mode will exit respective mode
 -- and press F5 in normal mode
@@ -20,28 +26,15 @@ vim.keymap.set({ "i", "v" }, "<F5>", "<ESC><F5>", { buffer = true, remap = true 
 
 -- pressing f8 will run the executable
 if vim.fn.has("win32") == 1 then
-	if vim.o.shell == "cmd.exe" then
-		vim.keymap.set(
-			"n",
-			"<f8>",
-			":<C-u>cd %:p:h<BAR>:exec 'term' shellescape(expand('%<')..'.exe')<CR>",
-			{ buffer = true }
-		)
-	else
-		vim.keymap.set(
-			"n",
-			"<f8>",
-			":<C-u>cd %:p:h<BAR>:exec 'term &' shellescape(expand('%<')..'.exe')<CR>",
-			{ buffer = true }
-		)
-	end
+	vim.keymap.set("n", "<f8>", function()
+		vim.cmd.cd("%:p:h")
+		vim.cmd.exec("'term' shellescape('bin/' .. expand('%<')..'.exe')")
+	end, { buffer = true })
 else
-	vim.keymap.set(
-		"n",
-		"<f8>",
-		":<C-u>cd %:p:h<BAR>:exec 'term' shellescape('./'..expand('%<'))<CR>",
-		{ buffer = true }
-	)
+	vim.keymap.set("n", "<f8>", function()
+		vim.cmd.cd("%:p:h")
+		vim.cmd.exec("'term' shellescape('bin/'..expand('%<'))")
+	end, { buffer = true })
 end
 
 -- just like above, pressing F8 in insert mode or visual mode will exit respective
@@ -53,7 +46,7 @@ vim.keymap.set({ "i", "v" }, "<F8>", "<ESC><F8>", { buffer = true, remap = true 
 -- vim.env.CFLAGS = "-Wall -Wextra -pedantic -g"
 -- vim.env.CXXFLAGS = "-Wall -Wextra -pedantic -g -std=c++17"
 -- cl flags
-vim.env.CFLAGS = "/Wall /Zi /Za"
+vim.env.CFLAGS = "/Wall /Zi"
 vim.env.CXXFLAGS = "/EHsc /Wall /wd4668 /Zi /std:c++20"
 
 vim.env.CC = "cl.exe"
