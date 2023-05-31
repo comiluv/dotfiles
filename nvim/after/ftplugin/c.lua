9134da62465ef1272ca90092f43b8c32a7823e7b
comiluv <choijaeho@gmail.com>
2023-06-01 00:27:51 +0900

    update c ftplugin

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -1,7 +1,7 @@
 -- clang-format default
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
-vim.bo.softtabstop = 4
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.softtabstop = 2
 vim.bo.expandtab = true
 
 -- it'll also be used for C++
