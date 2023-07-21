8d782d186a0c665f249bb24507a19ff572dd9a39
comiluv <choijaeho@gmail.com>
2023-07-21 22:54:16 +0900

    fix vcvars config failing in MSYS2

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -29,11 +29,13 @@ return {
 		config = function()
 			-- refernce: https://stackoverflow.com/questions/14369032/how-do-i-set-up-vim-to-compile-using-visual-studio-2010s-c-compiler
 			vim.cmd([[
-			let vcvars_dict = vcvars#get(vcvars#list()[-1])
-			let $PATH = join(vcvars_dict.path, ';') .. ';' .. $PATH
-			let $INCLUDE = join(vcvars_dict.include, ';') .. ';' .. $INCLUDE
-			let $LIB = join(vcvars_dict.lib, ';') .. ';' .. $LIB
-			let $LIBPATH = join(vcvars_dict.libpath, ';') .. ';' .. $LIBPATH
+			if (len(vcvars#list()) > 0)
+				let vcvars_dict = vcvars#get(vcvars#list()[-1])
+				let $PATH = join(vcvars_dict.path, ';') .. ';' .. $PATH
+				let $INCLUDE = join(vcvars_dict.include, ';') .. ';' .. $INCLUDE
+				let $LIB = join(vcvars_dict.lib, ';') .. ';' .. $LIB
+				let $LIBPATH = join(vcvars_dict.libpath, ';') .. ';' .. $LIBPATH
+			endif
 			]])
 		end,
 	},
