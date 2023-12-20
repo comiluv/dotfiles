2589c31fcdcb5fcc3e2fa0bae28c809ffb77d003
comiluv <choijaeho@gmail.com>
2023-12-20 22:41:37 +0900

    change neovide setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,6 +13,7 @@ if vim.g.neovide then
 	vim.g.neovide_refresh_rate_idle = 5
 	-- Turn off cursor animation completely
 	vim.g.neovide_cursor_animation_length = 0
+	vim.g.neovide_hide_mouse_when_typing = true
 end
 
 vim.opt.shadafile = ""
