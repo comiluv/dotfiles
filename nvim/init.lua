5a1de132057d77665f57394890266265a3ffc07f
comiluv <choijaeho@gmail.com>
2023-12-20 15:19:08 +0900

    neovide config

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,5 +7,12 @@ require("02_remap")
 require("03_autocmd")
 require("04_load_lazy")
 
-vim.opt.shadafile = ""
+if vim.g.neovide then
+	vim.o.guifont = "Liga SFMono Nerd Font,D2CodingLigature Nerd Font:h11"
+	vim.g.neovide_refresh_rate = 144
+	vim.g.neovide_refresh_rate_idle = 5
+	-- Turn off cursor animation completely
+	vim.g.neovide_cursor_animation_length = 0
+end
 
+vim.opt.shadafile = ""
