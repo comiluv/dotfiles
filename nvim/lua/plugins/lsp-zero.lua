cf895f7f5bbd7d58b12cdec79f64e342500109c0
comiluv <choijaeho@gmail.com>
2024-07-21 06:00:57 +0900

    remove lsp-inlayhints.nvim plugin
    no longer needed from 0.10

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -714,30 +714,4 @@ return {
 		ft = { "rust", "toml" },
 		config = true,
 	},
-
-	-- inlay hints feature that's going to be native in Neovim 0.10 but not yet in 0.9
-	{
-		"lvimuser/lsp-inlayhints.nvim",
-		event = { "LspAttach" },
-		config = function()
-			local inlay = require("lsp-inlayhints")
-			vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = "LspAttach_inlayhints",
-				callback = function(args)
-					if not (args.data and args.data.client_id) then
-						return
-					end
-
-					local bufnr = args.buf
-					local client = vim.lsp.get_client_by_id(args.data.client_id)
-					inlay.on_attach(client, bufnr)
-				end,
-			})
-			vim.keymap.set("n", "<leader>h", function()
-				inlay.toggle()
-			end, { desc = "Toggle inlay hints" })
-			inlay.setup()
-		end,
-	},
 }
