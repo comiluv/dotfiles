59575155687cea4746370199058f13c9a87e71e0
comiluv <choijaeho@gmail.com>
2024-07-27 17:28:30 +0900

    up bootstrap.sh

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/sh
 
+# disabling password
+# ask for username (which is current user)
+echo Input current username
+read username
+# add the username to a sudo config file
+temp=$(mktemp)
+printf '%s ALL=(ALL) NOPASSWD:ALL\n' "$username" > $temp
+chmod 644 $temp
+sudo cp $temp /etc/sudoers.d/$username
+
 # make sure to run this script as user after doing chmod +x filename
 cd ~
 
