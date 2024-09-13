128243cab7699df0419857858f880d70647d5049
comiluv <choijaeho@gmail.com>
2024-09-13 20:06:43 +0900

    install avante.nvim
    clean plugin structure

---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,127 @@
+return {
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- set this if you want to always pull the latest change
+		opts = {
+			-- add any opts here
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		-- build = "make",
+		build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false", -- for windows
+		dependencies = {
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
+
+	{
+		"zbirenbaum/copilot.lua",
+		event = "InsertEnter",
+		cmd = "Copilot",
+		build = ":Copilot auth",
+		config = function()
+			local function array_to_table(arr)
+				local tbl = {}
+				for _, v in ipairs(arr) do
+					tbl[v] = false
+				end
+				return tbl
+			end
+			-- https://codeinthehole.com/tips/vim-and-github-copilot/
+			local copilot_enabled_filetypes = {
+				gitcommit = true,
+				markdown = true,
+				yaml = true,
+			}
+			local filetypes = array_to_table(vim.g.info_file_pattern)
+			for k, v in pairs(copilot_enabled_filetypes) do
+				filetypes[k] = v
+			end
+			require("copilot").setup({
+				suggestion = {
+					auto_trigger = true,
+					keymap = {
+						accept = false,
+					},
+				},
+				filetypes = filetypes,
+			})
+			-- detach Copilot for big files
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("CopilotFileSizeCheck", {}),
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if client.name == "copilot" then
+						local file_size = vim.fn.getfsize(args.file)
+						if file_size > 100000 or file_size == -2 then
+							vim.defer_fn(function()
+								vim.cmd.Copilot("detach")
+							end, 0)
+						end
+					end
+				end,
+			})
+			-- start Copilot disabled
+			vim.cmd.Copilot("disable")
+		end,
+	},
+
+	-- AI assisted diagnostics
+	{
+		"piersolenski/wtf.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		opts = {
+			openai_model_id = "gpt-4o-mini",
+		},
+		keys = {
+			{
+				"gw",
+				mode = { "n", "x" },
+				function()
+					require("wtf").ai()
+				end,
+				desc = "Debug diagnostic with AI",
+			},
+			{
+				mode = { "n" },
+				"gW",
+				function()
+					require("wtf").search()
+				end,
+				desc = "Search diagnostic with Google",
+			},
+		},
+	},
+}
--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -668,35 +668,6 @@ return {
 		},
 	},
 
-	-- AI assisted diagnostics
-	{
-		"piersolenski/wtf.nvim",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-		},
-		opts = {
-			openai_model_id = "gpt-4o-mini",
-		},
-		keys = {
-			{
-				"gw",
-				mode = { "n", "x" },
-				function()
-					require("wtf").ai()
-				end,
-				desc = "Debug diagnostic with AI",
-			},
-			{
-				mode = { "n" },
-				"gW",
-				function()
-					require("wtf").search()
-				end,
-				desc = "Search diagnostic with Google",
-			},
-		},
-	},
-
 	{
 		"mrcjkb/rustaceanvim",
 		version = "^4",
