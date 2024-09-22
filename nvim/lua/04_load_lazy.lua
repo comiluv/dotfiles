d931cbb7c44a68282b64e2468e09daffd7c6d5a9
comiluv <choijaeho@gmail.com>
2024-09-22 14:27:00 +0900

    disable lazy.nvim checker

--- nvim/lua/04_load_lazy.lua
+++ nvim/lua/04_load_lazy.lua
@@ -20,7 +20,6 @@ vim.opt.rtp:prepend(lazypath)
 local opts = {
 	spec = { import = "plugins" },
 	change_detection = { notify = false },
-	checker = { enabled = true },
 	performance = {
 		rtp = {
 			disabled_plugins = {
