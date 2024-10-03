7d655f663c803b3448bc6927f94f1f4071140e2a
comiluv <choijaeho@gmail.com>
2024-10-04 08:19:27 +0900

    fix autocmd

--- nvim/lua/03_autocmd.lua
+++ nvim/lua/03_autocmd.lua
@@ -88,6 +88,10 @@ autocmd("BufWritePre", {
 	group = TrimGroup,
 	pattern = "*",
 	callback = function()
+		-- Only trim if the buffer is modifiable
+		if not vim.opt_local.modifiable:get() then
+			return
+		end
 		-- Only trim when there's no formatter attached
 		vim.b.num_formatters = vim.b.num_formatters or #(require("conform").list_formatters())
 		if vim.b.num_formatters > 0 then
@@ -157,27 +161,3 @@ autocmd({ "LspAttach" }, {
 		end
 	end,
 })
-
--- Make ClearShada user command to workaround Neovim creating endlessly empty main.shada.tmp.X files in $ENV:LocalAppData/nvim-data/shada
--- https://github.com/neovim/neovim/issues/8587#issuecomment-2176399196
-vim.api.nvim_create_user_command("ClearShada", function()
-	local shada_path = vim.fn.expand(vim.fn.stdpath("data") .. "/shada")
-	local files = vim.fn.glob(shada_path .. "/*", false, true)
-	local all_success = 0
-	for _, file in ipairs(files) do
-		local file_name = vim.fn.fnamemodify(file, ":t")
-		if file_name == "main.shada" then
-			-- skip your main.shada file
-			goto continue
-		end
-		local success = vim.fn.delete(file)
-		all_success = all_success + success
-		if success ~= 0 then
-			vim.notify("Couldn't delete file '" .. file_name .. "'", vim.log.levels.WARN)
-		end
-		::continue::
-	end
-	if all_success == 0 then
-		vim.print("Successfully deleted all temporary shada files")
-	end
-end, { desc = "Clears all the .tmp shada files" })
