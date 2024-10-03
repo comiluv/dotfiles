dea424230fff3e9029c6c72c224d7dd280bf1248
comiluv <choijaeho@gmail.com>
2024-10-04 02:57:10 +0900

    revert 6128f73e

--- bootstrap-deb.sh
+++ bootstrap-deb.sh
@@ -59,14 +59,8 @@ git config --global init.defaultbranch "main"
 
 git clone https://github.com/comiluv/dotfiles ~/dotfiles
 
-# Install mise https://github.com/jdx/mise
-apt update -y && apt install -y gpg sudo wget curl
-sudo install -dm 755 /etc/apt/keyrings
-wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
-sudo apt update
-sudo apt install -y mise
-echo 'export PATH="$HOME/.local/share/mise/shims:$PATH"' >> ~/.zprofile
+# Install volta
+curl https://get.volta.sh | bash
 
 # setup vim
 mkdir -p ~/.vim
