f803c5dd51729ec5cb40f9726b61ef658a44c37c
comiluv <choijaeho@gmail.com>
2024-10-05 23:06:33 +0900

    fix lazy loading

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -2,7 +2,7 @@ return {
 	-- automatically create any non-existent directories
 	{
 		"jghauser/mkdir.nvim",
-		event = "BufEnter",
+		event = "VeryLazy",
 		-- "BufWritePre", "FileWritePre", "BufWriteCmd", "BufModifiedSet", didnt work
 	},
 
