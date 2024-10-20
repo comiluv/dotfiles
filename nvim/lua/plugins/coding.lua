43f4610d1a9c8cac39e5ba765ddb723e10b042db
comiluv <choijaeho@gmail.com>
2024-10-21 07:51:14 +0900

    fix luasnip build command

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -188,10 +188,9 @@ return {
 			})
 		end,
 		-- jsregexp setup see https://github.com/L3MON4D3/LuaSnip/issues/1190#issuecomment-2171656749
-		build = vim.fn.executable("make") == 1 and "make install_jsregexp"
-			or (vim.fn.has("win32") == 1 and vim.fn.executable("pwsh") == 1) and ("pwsh -NoProfile " .. vim.fn.stdpath(
-				"config"
-			) .. "/luasnip_jsregexp_build.ps1")
+		build = (vim.fn.has("win32") == 1 and vim.fn.executable("pwsh") == 1)
+				and ("pwsh -NoProfile " .. vim.fn.stdpath("config") .. "/luasnip_jsregexp_build.ps1")
+			or vim.fn.executable("make") == 1 and "make install_jsregexp"
 			or "",
 	},
 
