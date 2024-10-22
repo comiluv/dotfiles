1c7ae9e5672899c504377c335ffff0c4eb61a334
comiluv <choijaeho@gmail.com>
2024-10-23 08:33:25 +0900

    fix nvim-autopairs config
    see https://github.com/windwp/nvim-autopairs/issues/417

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -226,40 +226,11 @@ return {
 		event = "InsertEnter",
 		config = function()
 			local npairs = require("nvim-autopairs")
-			-- use tree-sitter for nvim-autopairs
-			local Rule = require("nvim-autopairs.rule")
 
+			-- Press <A-e> in insert mode for fast wrap
 			npairs.setup({
-				check_ts = true,
-				ts_config = {},
 				fast_wrap = {},
 			})
-
-			-- Add spaces between parentheses
-			local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
-			npairs.add_rules({
-				Rule(" ", " "):with_pair(function(opts)
-					local pair = opts.line:sub(opts.col - 1, opts.col)
-					return vim.tbl_contains({
-						brackets[1][1] .. brackets[1][2],
-						brackets[2][1] .. brackets[2][2],
-						brackets[3][1] .. brackets[3][2],
-					}, pair)
-				end),
-			})
-			for _, bracket in pairs(brackets) do
-				npairs.add_rules({
-					Rule(bracket[1] .. " ", " " .. bracket[2])
-						:with_pair(function()
-							return false
-						end)
-						:with_move(function(opts)
-							return opts.prev_char:match(".%" .. bracket[2]) ~= nil
-						end)
-						:use_key(bracket[2]),
-				})
-			end
-
 			-- If you want insert `(` after select function or method item
 			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 			local cmp = require("cmp")
