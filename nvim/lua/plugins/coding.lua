a5cf887dde0babd583de68599f89e45d92ff0bb9
comiluv <choijaeho@gmail.com>
2024-10-24 13:52:30 +0900

    fix coq_nvim keymaps and comments

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -21,6 +21,7 @@ return {
 				keymap = { recommended = false },
 				display = { statusline = { helo = false } },
 			}
+
 			-- Keybindings
 			local has_copilot, copilot = pcall(require, "copilot.suggestion")
 			local has_tabout, _ = pcall(require, "tabout")
@@ -30,10 +31,12 @@ return {
 				vim.keymap.set(mode, lh, rh, { expr = true, silent = true })
 			end
 
+			-- https://github.com/abecodes/tabout.nvim?tab=readme-ov-file#more-complex-keybindings
 			local termcode = function(key)
 				return vim.api.nvim_replace_termcodes(key, true, true, false)
 			end
 
+			-- Pop up menu item if none selected, select pop up menu item if any selected, Copilot autocompletion, Tabout Plugin Tab, raw Tab
 			local tab_key = function()
 				if vim.fn.pumvisible() ~= 0 then
 					if vim.fn.complete_info().selected == -1 then
@@ -50,6 +53,7 @@ return {
 				end
 			end
 
+			-- Previous pop up menu item, Tabout Plugin Back, raw S-Tab
 			local shift_tab_key = function()
 				if vim.fn.pumvisible() ~= 0 then
 					return termcode("<C-p>")
@@ -60,6 +64,7 @@ return {
 				end
 			end
 
+			-- Accept pop up menu item if selected, otherwise close pop up menu and send CR
 			local cr_key = function()
 				if vim.fn.pumvisible() ~= 0 then
 					if vim.fn.complete_info({ "selected" }).selected ~= -1 then
@@ -73,6 +78,7 @@ return {
 				end
 			end
 
+			-- Close pop up menu and send BS
 			local bs_key = function()
 				if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ "mode" }).mode == "eval" then
 					return termcode("<c-e>") .. termcode("<bs>")
@@ -81,20 +87,44 @@ return {
 				end
 			end
 
+			-- Scroll down pop up menu for at most 8 items
+			local ctrl_d_key = function()
+				if vim.fn.pumvisible ~= 0 then
+					local result = ""
+					local current_pos = vim.fn.complete_info({ "selected" }).selected
+					local num_items = #vim.fn.complete_info({ "items" }).items - 1 -- Convert 1 based index to 0 based index
+					local repeats = current_pos + 8 > num_items and num_items - current_pos or 8
+					for _ = 1, repeats do
+						result = result .. "<C-n>"
+					end
+					return termcode(result)
+				else
+					return termcode("<C-d>")
+				end
+			end
+
+			-- Scroll up pop up menu for at most 8 items
+			local ctrl_u_key = function()
+				if vim.fn.pumvisible ~= 0 then
+					local result = ""
+					local current_pos = vim.fn.complete_info({ "selected" }).selected
+					local repeats = current_pos < 8 and current_pos or 8
+					for _ = 1, repeats do
+						result = result .. "<C-p>"
+					end
+					return termcode(result)
+				else
+					return termcode("<C-u>")
+				end
+			end
+
+			-- Close pop up menu and send ESC
 			map("<Esc>", [[pumvisible() ? "\<C-e><Esc>" : "\<Esc>"]])
+			-- Close pop up menu and send C-c
 			map("<C-c>", [[pumvisible() ? "\<C-e><C-c>" : "\<C-c>"]])
-			vim.keymap.set(
-				"i",
-				"<C-d>",
-				[[pumvisible() ? "\<C-n><C-n><C-n><C-n><C-n><C-n><C-n><C-n>" : "\<C-d>"]],
-				{ noremap = false, silent = true, expr = true }
-			)
-			vim.keymap.set(
-				"i",
-				"<C-u>",
-				[[pumvisible() ? "\<C-p><C-p><C-p><C-p><C-p><C-p><C-p><C-p>" : "\<C-u>"]],
-				{ noremap = false, silent = true, expr = true }
-			)
+
+			map("<C-d>", ctrl_d_key)
+			map("<C-u>", ctrl_u_key)
 
 			map("<BS>", bs_key)
 			map("<CR>", cr_key)
