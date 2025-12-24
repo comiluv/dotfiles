33a0b6a31fc3e8063aa954e976d7547c01176172
comiluv <choijaeho@gmail.com>
2025-12-25 00:45:45 +0900

    refactor treesitter related config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,41 +1,3 @@
--- from https://github.com/cameronr/dotfiles/blob/main/nvim/lua/plugins/treesitter.lua
--- on main branch, treesitter isn't started automatically
-vim.api.nvim_create_autocmd({ "Filetype" }, {
-	callback = function(event)
-		if vim.tbl_contains(vim.g.info_filetype, event.match) then
-			return
-		end
-
-		-- skip for large files
-		local max_filesize = 100 * 1024 -- 100 KB
-		local filesize_ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(event.buf))
-		if not filesize_ok or stats and stats.size > max_filesize then
-			return true
-		end
-
-		-- make sure nvim-treesitter is loaded
-		local ok, nvim_treesitter = pcall(require, "nvim-treesitter")
-
-		-- no nvim-treesitter, maybe fresh install
-		if not ok then
-			return
-		end
-
-		local ft = vim.bo[event.buf].ft
-		local lang = vim.treesitter.language.get_lang(ft)
-		nvim_treesitter.install({ lang }):await(function(err)
-			if err then
-				vim.notify("Treesitter install error for ft: " .. ft .. " err: " .. err)
-				return
-			end
-
-			pcall(vim.treesitter.start, event.buf)
-			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-		end)
-	end,
-})
-
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -52,6 +14,37 @@ return {
 		config = function(_, opts)
 			local ensure_installed = {}
 			require("nvim-treesitter").install(ensure_installed)
+			vim.api.nvim_create_autocmd({ "Filetype" }, {
+				callback = function(event)
+					if vim.tbl_contains(vim.g.info_filetype, event.match) then
+						return
+					end
+					-- skip for large files
+					local max_filesize = 100 * 1024 -- 100 KB
+					local filesize_ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(event.buf))
+					if not filesize_ok or stats and stats.size > max_filesize then
+						return true
+					end
+					-- make sure nvim-treesitter is loaded
+					local ok, nvim_treesitter = pcall(require, "nvim-treesitter")
+					-- no nvim-treesitter, maybe fresh install
+					if not ok then
+						return
+					end
+					local ft = vim.bo[event.buf].ft
+					local lang = vim.treesitter.language.get_lang(ft)
+					nvim_treesitter.install({ lang }):await(function(err)
+						if err then
+							vim.notify("Treesitter install error for ft: " .. ft .. " err: " .. err)
+							return
+						end
+
+						pcall(vim.treesitter.start, event.buf)
+						vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+						vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					end)
+				end,
+			})
 		end,
 	},
 
