00c467b928f62bdbc3fa1c8fc37090490a478c61
comiluv <choijaeho@gmail.com>
2025-12-31 03:42:25 +0900

    add plugin dependency for lazygit.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"kdheepak/lazygit.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		cmd = "LazyGit",
 		keys = {
 			{ "<leader>gs", vim.cmd.LazyGit, silent = true, desc = [[LazyGit]] },
