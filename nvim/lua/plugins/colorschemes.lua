0583342cc27639269054ce658fe828b68bf344ca
comiluv <choijaeho@gmail.com>
2025-12-31 03:09:32 +0900

    remove other colorschemes

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,11 +1,5 @@
 return {
 	{
-		"navarasu/onedark.nvim",
-	},
-	{
-		"NTBBloodbath/doom-one.nvim",
-	},
-	{
 		"projekt0n/github-nvim-theme",
 		name = "github-theme",
 		priority = 1000,
@@ -13,17 +7,5 @@ return {
 			vim.opt.background = "light"
 			vim.cmd.colorscheme("github_light")
 		end,
-	},
-	{
-		"p00f/alabaster.nvim",
-	},
-	{
-		"miikanissi/modus-themes.nvim",
-	},
-	{
-		"polirritmico/monokai-nightasty.nvim",
-	},
-	{
-		"thimc/gruber-darker.nvim",
 	},
 }
