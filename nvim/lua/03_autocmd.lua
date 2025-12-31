ef0a9b2c5ae94d26ea21a5e14c8daf08335d5e0f
comiluv <choijaeho@gmail.com>
2026-01-01 04:03:22 +0900

    get some autocmds from LazyVim

--- nvim/lua/03_autocmd.lua
+++ nvim/lua/03_autocmd.lua
@@ -257,7 +257,7 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
 -- Make sure ShaDa temp files that are empty are deleted on exit
 -- https://github.com/neovim/neovim/issues/8587#issuecomment-3557794273
 vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
-	group = vim.api.nvim_create_augroup("fuck_shada_temp", { clear = true }),
+	group = augroup("fuck_shada_temp", { clear = true }),
 	pattern = { "*" },
 	callback = function()
 		local status = 0
@@ -272,4 +272,51 @@ vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
 		end
 	end,
 	desc = "Delete empty temp ShaDa files",
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+	group = augroup("resize_splits", {}),
+	callback = function()
+		local current_tab = vim.fn.tabpagenr()
+		vim.cmd("tabdo wincmd =")
+		vim.cmd("tabnext " .. current_tab)
+	end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = augroup("last_loc", {}),
+	callback = function(event)
+		local exclude = { "gitcommit" }
+		local buf = event.buf
+		if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+			return
+		end
+		vim.b[buf].lazyvim_last_loc = true
+		local mark = vim.api.nvim_buf_get_mark(buf, '"')
+		local lcount = vim.api.nvim_buf_line_count(buf)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
+-- make it easier to close man-files when opened inline
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("man_unlisted", {}),
+	pattern = { "man" },
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+	end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("wrap_spell", {}),
+	pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
 })
