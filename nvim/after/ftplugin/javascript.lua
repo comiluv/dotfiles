5f261c3e511b465990b6d8048bdfc941aa4a32f9
comiluv <choijaeho@gmail.com>
2026-02-25 02:29:18 +0900

    fix comment

--- nvim/after/ftplugin/javascript.lua
+++ nvim/after/ftplugin/javascript.lua
@@ -9,7 +9,7 @@ if vim.g.loaded_node_provider == 0 then
 	return
 end
 
--- pressing f8 will run the file and skip vim.fn.has("node") test for performance
+-- pressing f8 will skip vim.fn.executable("node") test and run the file
 vim.keymap.set("n", "<F8>", ":<C-u>update<BAR>:cd %:p:h<BAR>:exec 'term node' shellescape(@%)<CR>", { buffer = true })
 
 -- pressing F8 in insert mode or visual mode will exit respective mode and press F8
