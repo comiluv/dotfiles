ca5eb6f2930f9fad3f195f9a6a601d13388a7869
comiluv <choijaeho@gmail.com>
2026-02-25 03:06:30 +0900

    remove ClearShada user command

--- nvim/lua/05_user_command.lua
+++ nvim/lua/05_user_command.lua
@@ -57,30 +57,6 @@ vim.keymap.set("ca", "grepadd", "Grepadd")
 vim.keymap.set("ca", "lgrep", "LGrep")
 vim.keymap.set("ca", "lgrepadd", "LGrepadd")
 
--- Make ClearShada user command to workaround Neovim creating endlessly empty main.shada.tmp.X files in $ENV:LocalAppData/nvim-data/shada
--- https://github.com/neovim/neovim/issues/8587#issuecomment-2176399196
-usercmd("ClearShada", function()
-	local shada_path = vim.fn.expand(vim.fn.stdpath("data") .. "/shada")
-	local files = vim.fn.glob(shada_path .. "/*", false, true)
-	local all_success = 0
-	for _, file in ipairs(files) do
-		local file_name = vim.fn.fnamemodify(file, ":t")
-		if file_name == "main.shada" then
-			-- skip your main.shada file
-			goto continue
-		end
-		local success = vim.fn.delete(file)
-		all_success = all_success + success
-		if success ~= 0 then
-			vim.notify("Couldn't delete file '" .. file_name .. "'", vim.log.levels.WARN)
-		end
-		::continue::
-	end
-	if all_success == 0 then
-		vim.notify("Successfully deleted all temporary shada files", vim.log.levels.INFO)
-	end
-end, { desc = "Clears all the .tmp shada files" })
-
 -- Close all buffers except the current one
 -- Usage: :BufOnly
 usercmd("BufOnly", function()
